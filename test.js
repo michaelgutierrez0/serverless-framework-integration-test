9db13a9f002ebac287613178264f5c8cb0ef5ac7
Gutierrez <michaelg@a483e7be3c67.ant.amazon.com>
2020-10-27 19:52:42 -0500

    Initial push

---
+++ test.js
@@ -0,0 +1,13 @@
+module.exports.hello = async (event, context) => {
+    let body = {};
+    if (event.body) {
+      body = JSON.parse(event.body)
+    }
+  
+    const name = body.name || 'world'
+  
+    return {
+      statusCode: 200,
+      body: `Hello, ${name}`,
+    };
+  };
